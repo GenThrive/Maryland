b8dabc4e4ba1432cc5194dd9891184cd249fbd92
GenThrive <genthrive@ecorise.org>
2024-10-18 12:29:56 -0500

    added all files

--- styling.py
+++ styling.py
@@ -1,8 +1,8 @@
 # ----------------------------------------------------------------------------
 # PAGE SETTINGS
 # ----------------------------------------------------------------------------
-app_title = 'Wisconsin Environmental Education Directory'
-page_title = 'Wisconsin Environmental Education Directory'
+app_title = 'Maryland Environmental Education Directory'
+page_title = 'Maryland Environmental Education Directory'
 sub_title = 'Filter on'
 filter_category_1 = 'Organization Data'
 filter_category_2 = 'Program Data'
@@ -10,8 +10,8 @@ filter_category_2 = 'Program Data'
 # ----------------------------------------------------------------------------
 # MAP SETTINGS
 # ----------------------------------------------------------------------------
-map_center_lat = 44.8742713
-map_center_lon = -91.2084173
+map_center_lat = 38.797406
+map_center_lon = -78.5559902
 map_zoom = 5.072
 
 # Column in the dataset that maps to the featureidkey of the geojson.
